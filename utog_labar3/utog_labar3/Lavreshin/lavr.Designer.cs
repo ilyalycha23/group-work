43bc3baadff3d00f49f3a5a358a3da1767b0ec8c
ilyalycha <ilyalav2323@gmail.com>
2025-05-30 22:17:02 +0300

    Реализация основного скрипта
    Создание методов:
     - Настройка внешнего вида графика
     - Загрузка данных из JSON
     - Отображение данных в таблице
     - Обновление графика
     - Расчет статистики
     - Расчет прогноза

--- utog_labar3/utog_labar3/Lavreshin/lavr.Designer.cs
+++ utog_labar3/utog_labar3/Lavreshin/lavr.Designer.cs
@@ -35,13 +35,13 @@ namespace utog_labar3.Lavreshin
             this.label1 = new System.Windows.Forms.Label();
             this.comboBoxApartment = new System.Windows.Forms.ComboBox();
             this.btnLoadData = new System.Windows.Forms.Button();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartPrices = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridPrices = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtForecastYears = new System.Windows.Forms.TextBox();
             this.btnForecast = new System.Windows.Forms.Button();
             this.lblResults = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPrices)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridPrices)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,6 +65,7 @@ namespace utog_labar3.Lavreshin
             this.comboBoxApartment.Name = "comboBoxApartment";
             this.comboBoxApartment.Size = new System.Drawing.Size(121, 21);
             this.comboBoxApartment.TabIndex = 1;
+            this.comboBoxApartment.SelectedIndexChanged += new System.EventHandler(this.comboBoxApartment_SelectedIndexChanged);
             // 
             // btnLoadData
             // 
@@ -74,22 +75,23 @@ namespace utog_labar3.Lavreshin
             this.btnLoadData.TabIndex = 2;
             this.btnLoadData.Text = "Загрузить данные";
             this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
             // 
-            // chart1
+            // chartPrices
             // 
             chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
+            this.chartPrices.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(12, 192);
-            this.chart1.Name = "chart1";
+            this.chartPrices.Legends.Add(legend2);
+            this.chartPrices.Location = new System.Drawing.Point(12, 192);
+            this.chartPrices.Name = "chartPrices";
             series2.ChartArea = "ChartArea1";
             series2.Legend = "Legend1";
             series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(388, 150);
-            this.chart1.TabIndex = 3;
-            this.chart1.Text = "chart1";
+            this.chartPrices.Series.Add(series2);
+            this.chartPrices.Size = new System.Drawing.Size(388, 150);
+            this.chartPrices.TabIndex = 3;
+            this.chartPrices.Text = "chart1";
             // 
             // dataGridPrices
             // 
@@ -108,12 +110,12 @@ namespace utog_labar3.Lavreshin
             this.label2.TabIndex = 5;
             this.label2.Text = "Введите число лет (n):";
             // 
-            // textBox1
+            // txtForecastYears
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 348);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 6;
+            this.txtForecastYears.Location = new System.Drawing.Point(137, 348);
+            this.txtForecastYears.Name = "txtForecastYears";
+            this.txtForecastYears.Size = new System.Drawing.Size(121, 20);
+            this.txtForecastYears.TabIndex = 6;
             // 
             // btnForecast
             // 
@@ -123,6 +125,7 @@ namespace utog_labar3.Lavreshin
             this.btnForecast.TabIndex = 7;
             this.btnForecast.Text = "Рассчитать прогноз";
             this.btnForecast.UseVisualStyleBackColor = true;
+            this.btnForecast.Click += new System.EventHandler(this.btnForecast_Click);
             // 
             // lblResults
             // 
@@ -140,16 +143,16 @@ namespace utog_labar3.Lavreshin
             this.ClientSize = new System.Drawing.Size(410, 443);
             this.Controls.Add(this.lblResults);
             this.Controls.Add(this.btnForecast);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtForecastYears);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridPrices);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.chartPrices);
             this.Controls.Add(this.btnLoadData);
             this.Controls.Add(this.comboBoxApartment);
             this.Controls.Add(this.label1);
             this.Name = "lavr";
             this.Text = "lavr";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPrices)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridPrices)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -161,10 +164,10 @@ namespace utog_labar3.Lavreshin
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBoxApartment;
         private System.Windows.Forms.Button btnLoadData;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPrices;
         private System.Windows.Forms.DataGridView dataGridPrices;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtForecastYears;
         private System.Windows.Forms.Button btnForecast;
         private System.Windows.Forms.Label lblResults;
     }
