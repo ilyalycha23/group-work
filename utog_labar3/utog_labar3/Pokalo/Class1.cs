46d02c370edcde53b1c6d0f8058b5f540c6262ea
rostislavguap <rostislavsambo@gmail.com>
2025-05-31 00:23:46 +0300

    Добавление элементов в форму и разработка функциональности кнопок.
    На форму добавлены необходимые элементы. В коде формы прописан функционал для всех его элементов.

--- utog_labar3/utog_labar3/Pokalo/Class1.cs
+++ utog_labar3/utog_labar3/Pokalo/Class1.cs
@@ -8,6 +8,7 @@ namespace utog_labar3.Pokalo
 {
     public static class MovingAverageCalculator
     {
+        // Основной метод для расчета скользящего среднего
         public static List<double> Calculate(List<double> data, int windowSize)
         {
             if (windowSize <= 0 || windowSize > data.Count)
@@ -28,6 +29,7 @@ namespace utog_labar3.Pokalo
             return movingAverages;
         }
 
+        // Стандартный метод прогноза (без учета тренда)
         public static List<double> Forecast(List<double> data, int windowSize, int periods)
         {
             if (data == null || data.Count == 0)
@@ -40,12 +42,10 @@ namespace utog_labar3.Pokalo
             {
                 double sum = 0;
                 int start = Math.Max(0, movingAverages.Count - windowSize);
-
                 for (int j = start; j < movingAverages.Count; j++)
                 {
                     sum += movingAverages[j];
                 }
-
                 double nextValue = sum / Math.Min(windowSize, movingAverages.Count);
                 forecast.Add(nextValue);
                 movingAverages.Add(nextValue);
@@ -53,6 +53,42 @@ namespace utog_labar3.Pokalo
 
             return forecast;
         }
+
+        // Улучшенный метод прогноза с учетом тренда
+        public static List<double> ForecastWithTrend(List<double> data, int windowSize, int periods)
+        {
+            if (data == null || data.Count == 0)
+                throw new ArgumentException("Нет данных для прогноза");
+
+            // Рассчитываем скользящие средние
+            List<double> movingAverages = Calculate(data, windowSize);
+
+            // Линейная регрессия для вычисления тренда (y = a*x + b)
+            double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+            int n = movingAverages.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                sumX += i;               // x (время)
+                sumY += movingAverages[i]; // y (значения)
+                sumXY += i * movingAverages[i];
+                sumX2 += i * i;
+            }
+
+            double a = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX); // Наклон тренда
+            double b = (sumY - a * sumX) / n;                                // Пересечение
+
+            // Прогнозируем с учетом тренда
+            List<double> forecast = new List<double>();
+            for (int i = 0; i < periods; i++)
+            {
+                double nextValue = a * (n + i) + b;
+                forecast.Add(nextValue);
+            }
+
+            return forecast;
+        }
     }
 }
+
 
--- utog_labar3/utog_labar3/Pokalo/pok.Designer.cs
+++ utog_labar3/utog_labar3/Pokalo/pok.Designer.cs
@@ -29,12 +29,143 @@ namespace utog_labar3.Pokalo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(218, 74);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(259, 27);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(516, 411);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Загрузить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(110, 27);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(110, 78);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Параметр N для скользящего среднего";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(9, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 40);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Количество лет для прогноза";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Построить прогноз";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 316);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 25);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Экспорт графика";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pok
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "pok";
             this.Text = "pok";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
